4207c95e09f0f3274c10151bc43684c6565e47e8
Masanari Doi <91431813+doimiod@users.noreply.github.com>
2023-06-05 10:40:50 +0100

    Update server.js

--- assignment1/codes/server.js
+++ assignment1/codes/server.js
@@ -22,7 +22,7 @@ app.listen(port, () => console.log(`weather forecast app listening on port ${por
 function sendWeatherInfo(req, res) {
 
     let city = req.params.city // input from user
-    var key = "fdd7644f33ac6e3c158e6ea9f1b9c526" // key
+    var key = "" // key
 
     // i used units=metric to get celcius
     axios("https://api.openweathermap.org/data/2.5/forecast?q="+city+"&units=metric&appid="+key)
